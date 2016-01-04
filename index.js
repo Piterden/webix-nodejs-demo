8c038cbfaf5c489f741b537bd21ebc73efb48057
Den Efremov <efremov.a.denis@gmail.com>
2016-01-04 04:05:42 +0300

    Second commit. Deps are wrong

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var path = require('path');
 
 //connect to the mongo
-var db = require('mongoskin').db("mongodb://localhost/testdb", { w: 0});
-	db.bind('events');
-	db.bind('persons');
-	db.bind('documents');
+var db = require('mongoskin').db("mongodb://localhost/testdb", {
+	w: 0
+});
+db.bind('events');
+db.bind('persons');
+db.bind('documents');
 
 //create the app instance
 var app = express();
@@ -16,22 +18,23 @@ app.use(express.bodyParser());
 
 
 //response for saving operations
-function after_update(err, res, events){
-    if (err){
-    	res.status(500);
-    	res.send({ error:err.toString() });
-    } else {
-    	res.send(events || {});
-    }
+function after_update(err, res, events) {
+	if (err) {
+		res.status(500);
+		res.send({
+			error: err.toString()
+		});
+	} else {
+		res.send(events || {});
+	}
 }
 
 
 //:entity loading
-app.get('/:entity', function(req, res){
-	console.log(req.param("entity"));
-	entity = req.param("entity");
-	db[req.param("entity")].find().toArray(function(err, data){
-		for (var i = 0; i < data.length; i++){
+app.get('/:entity', function(req, res) {
+	var coll = req.param("entity");
+	db[coll].find().toArray(function(err, data) {
+		for (var i = 0; i < data.length; i++) {
 			//map _id to id
 			data[i].id = data[i]._id;
 			delete data[i]._id;
@@ -41,28 +44,38 @@ app.get('/:entity', function(req, res){
 });
 
 //adding
-app.post('/:entity', function(req, res){
-	db.events.insert(req.body, function(err, events){
-		if (err) return res.send({ status:"error" });
-		res.send({ newid:req.body._id });
+app.post('/:entity', function(req, res) {
+	db[req.param("entity")].insert(req.body, function(err, events) {
+		console.log(err);
+		if (err) return res.send({
+			status: err
+		});
+		res.send({
+			newid: req.body._id
+		});
 	});
 });
 
 //updating
-app.put('/:entity/:id', function(req, res){
-	db.events.updateById(req.param("id"), req.body, function(err){
-		if (err) return res.send({ status:"error" });
+app.put('/:entity/:id', function(req, res) {
+	db[req.param("entity")].updateById(req.param("id"), req.body, function(err) {
+		if (err) return res.send({
+			status: err
+		});
 		res.send({});
 	});
 });
 
 //deleting
-app.delete('/:entity/:id', function(req, res){
-	db.events.removeById(req.param("id"), req.body, function(err){
-		if (err) return res.send({ status:"error" });
+app.delete('/:entity/:id', function(req, res) {
+	db[req.param("entity")].removeById(req.param("id"), req.body, function(err) {
+		if (err) return res.send({
+			status: err
+		});
 		res.send({});
 	});
 });
 
 
 app.listen(3000);
+
